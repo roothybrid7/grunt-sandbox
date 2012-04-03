8cc117ce84ce477f0a4a59cc5c2670327bb870d3
Satoshi Ohki <roothybrid7@gmail.com>
2012-04-03 13:01:13 +0900

    modified test snip.

--- js/snip/43-testscript.js
+++ js/snip/43-testscript.js
@@ -5,7 +5,7 @@
 $(function() {
   var bgColor = Math.floor(Math.random() * 0xffffff).toString(16);
   var color = (parseInt(bgColor, 16) ^ 0xffffff).toString(16);
-  var message = '43-testscript.js が組み込まれた模様!!';
+  var message = '43-testscript.js が読み込まれた模様!!';
 
   $('<div>' + message + '</div>').appendTo('#main')
     .css({backgroundColor: '#' + bgColor})
